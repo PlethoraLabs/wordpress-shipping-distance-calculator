b9ec6be1a3697b7a6bb4294850c589b39d4070b5
Kostas Minaidis <kostas@creativecollective.gr>
2016-05-15 03:04:07 +0200

    Adding public facing methods

--- public/class-ple-wsdc-public.php
+++ public/class-ple-wsdc-public.php
@@ -96,8 +96,69 @@ class Ple_Wsdc_Public {
 		 * class.
 		 */
 
+		/*** PLE-WSDC CODE ***/
+		wp_enqueue_script( 'js-cookie', plugin_dir_url( __FILE__ ) . 'js/js.cookie.min.js', array( 'jquery' ), $this->version, false );
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/ple-wsdc-public.js', array( 'jquery' ), $this->version, false );
 
 	}
+
+	/*** PLE-WSDC CODE ***/
+
+  // ADD ZIP CODE POKE SECTION 
+  public function plethora_add_markup_section($content) {
+
+    if ( is_product() || is_shop() || is_product_category() ) {
+
+      $content = 
+
+      '<div id="zipCodeWrapper">
+        <a href="#" id="zipCodeDismiss">Close</a>
+        <div class="plethora-zip-code-input">
+          <span>Please enter your zip code to receive shipping costs:</span>
+          <input id="zipCode" type="text" />
+          <input id="zipCodeSubmitter" type="submit" value="Submit" />
+        </div>
+      </div>';
+
+    } 
+
+    echo $content;
+
+  }
+
+  // DISPLAY SHIPPING COSTS 
+  public function add_custom_price_front($p, $obj) {
+
+    // $post_id = $obj->post->ID;
+
+    if ( isset($_COOKIE['zipCode']) ){
+
+      $zipCode = $_COOKIE['zipCode'];
+
+      // Do we already have this zip code stored?
+      if ( get_option( 'ple_wsdc_zipcode_' . $zipCode ) ){
+
+        $cost = get_option( 'ple_wsdc_zipcode_' . $zipCode );
+        $cost = $cost['cost'];
+
+      // Let's get the distance using Google API
+      } else {
+
+        $place_id = Ple_Wsdc_Helpers::zip_code_to_place_id( $zipCode, get_option( Ple_Wsdc_Helpers::$option_name . '_cc_tld' ) );
+        $distance = Ple_Wsdc_Helpers::get_distance( $place_id['place_id'], get_option('ple_wsdc_zipcode_destination'));
+        $cost     = Ple_Wsdc_Helpers::calc_distance_cost($distance);
+        update_option( 'ple_wsdc_zipcode_' . $zipCode, array( 'cost' => $cost, 'address' => $place_id['formatted_address']  ), '', 'yes' );
+
+      }
+
+      return $p . "<br /><span style='font-size:80%' class='pro_price_extra_info'> Shipping Costs: " . get_woocommerce_currency_symbol() . $cost . "</span>";
+
+    } else {
+
+      return $p;
+
+    }
+
+  }
 
 }
--- public/js/ple-wsdc-public.js
+++ public/js/ple-wsdc-public.js
@@ -1,32 +1,34 @@
 (function( $ ) {
+
 	'use strict';
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+	 $( window ).load(function() {
+
+			var $zipCodeWrapper    = $("#zipCodeWrapper");
+			var $zipCodeSubmitter  = $("#zipCodeSubmitter");
+			var $zipCodeInput      = $("#zipCode");
+			var $zipCodeDismissBtn = $("#zipCodeDismiss");
+
+      if ( $zipCodeWrapper.length ){
+
+        if ( !Cookies.get("zipCode") && !Cookies.get("zipCodeDismissed") ){
+
+          $zipCodeWrapper.css("display","block");
+          $zipCodeSubmitter.click(function(e){
+            e.preventDefault();
+            Cookies.set("zipCode", $zipCodeInput.val().replace(/ /g,""));
+            location.reload();
+          });
+
+          $zipCodeDismissBtn.click(function(e){
+            e.preventDefault();
+            Cookies.set("zipCodeDismissed","true");
+            $zipCodeWrapper.css( "display", "none");
+          });
+
+        }
+      }
+
+	 });
 
 })( jQuery );
